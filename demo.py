e4aed058fb445d1f57175dc8953ca8b12d283da7
Graham Troyer-Joy <graham.troyer_joy@stitchfix.com>
2020-10-19 10:25:26 -0400

    fix indentation

--- demo.py
+++ demo.py
@@ -8,11 +8,11 @@ with PiCamera() as camera:
   camera.framerate = 2.5
   camera.shutter_speed = 400000
   sleep(30) # Camera warm-up time
-    for i, filename in enumerate(camera.capture_continuous('/data/image{counter:02d}.jpg')):
-      print('Captured %s' % filename)
-        #sleep(1)
-        if i >= 59:
-          break
+  for i, filename in enumerate(camera.capture_continuous('/data/image{counter:02d}.jpg')):
+    print('Captured %s' % filename)
+    #sleep(1)
+    if i >= 59:
+      break
 
 print('Pictures taken')
 time.sleep(10)
