4f57b9ce12e47f5cc95b68326c49b1978ab15b54
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-10-18 22:56:56 -0400

    updates

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
+    # camera.resolution = (320, 240)
     # Camera warm-up time
     time.sleep(2)
     camera.capture('/data/image.jpg')
