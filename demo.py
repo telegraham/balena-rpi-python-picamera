326b0e72cf498c8afdf7c5f540951559332bfa42
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2020-10-18 23:16:17 -0400

    updaate

--- demo.py
+++ demo.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
-import time
-import picamera
+from picamera import PiCamera
+from time import sleep
+from fractions import Fraction
 
-with picamera.PiCamera() as camera:
-    # camera.resolution = (320, 240)
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('/data/image.jpg')
+with PiCamera() as camera:
+  camera.framerate = 2.5
+  camera.shutter_speed = 400000
+  sleep(30) # Camera warm-up time
+      for i, filename in enumerate(camera.capture_continuous('/data/image{counter:02d}.jpg')):
+          print('Captured %s' % filename)
+          #sleep(1)
+          if i >= 59:
+                break
 
-print('Picture taken')
+print('Pictures taken')
 time.sleep(10)
